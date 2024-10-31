deffe0da7a733df70982701521fa4abc9a274755
Hoàng <hoang21099@gmail.com>
2024-10-31 23:50:41 +0700

    handle grap valid tree using dfs and bfs

--- day7/graphValidTree.js
+++ day7/graphValidTree.js
@@ -15,15 +15,18 @@ var validTree = function (n, edges) {
 
   for (let [u, v] of edges) {
     adjList[u].push(v);
+    adjList[v].push(u); // Add this line to handle undirected edges
   }
 
   const visited = new Set();
 
-  const dfs = (node) => {
+  const dfs = (node, parent) => {
     if (visited.has(node)) return false;
     visited.add(node);
     for (let neighbor of adjList[node]) {
-      if (!dfs(neighbor)) {
+      if (neighbor === parent) continue; // Skip the edge leading back to the parent
+
+      if (!dfs(neighbor, node)) {
         return false;
       }
     }
@@ -34,21 +37,77 @@ var validTree = function (n, edges) {
   return !!dfs(0, -1);
 };
 
+const validTreeUsingStack = (n, edges) => {
+  if (n === 0) return false;
+
+  const adjList = new Array(n).fill(0).map(() => []);
+
+  for (let [u, v] of edges) {
+    adjList[u].push(v);
+    adjList[v].push(u); // Add this line to handle undirected edges
+  }
+
+  const visited = new Set();
+  const stack = [[0, -1]];
+
+  while (stack.length) {
+    const [node, parent] = stack.pop();
+
+    if (visited.has(node)) return false;
+    visited.add(node);
+    for (let neighbor of adjList[node]) {
+      if (neighbor === parent) continue;
+      if (visited.has(neighbor)) return false;
+
+      stack.push([neighbor, node]);
+    }
+  }
+  return true;
+};
+
+const validTreeUsingQueue = (n, edges) => {
+  if (n === 0) return false;
+
+  const adjList = new Array(n).fill(0).map(() => []);
+
+  for (let [u, v] of edges) {
+    adjList[u].push(v);
+    adjList[v].push(u); // Add this line to handle undirected edges
+  }
+
+  const visited = new Set();
+  const stack = [[0, -1]];
+
+  while (stack.length) {
+    const [node, parent] = stack.shift();
+
+    if (visited.has(node)) return false;
+    visited.add(node);
+    for (let neighbor of adjList[node]) {
+      if (neighbor === parent) continue;
+      if (visited.has(neighbor)) return false;
+
+      stack.push([neighbor, node]);
+    }
+  }
+  return true;
+};
+
 console.log(
-  validTree(5, [
+  validTreeUsingQueue(5, [
     [0, 1],
-    [0, 2],
+    [2, 0],
     [0, 3],
     [1, 4],
   ])
 ); // true
 
-console.log(
-  validTree(5, [
-    [0, 1],
-    [1, 2],
-    [2, 3],
-    [1, 3],
-    [1, 4],
-  ])
-); // false
+// console.log(
+//     validTreeUsingStack(5, [
+//     [0, 1],
+//     [1, 2],
+//     [2, 3],
+//     [1, 3],
+//     [1, 4],
+//   ])
+// ); // false
